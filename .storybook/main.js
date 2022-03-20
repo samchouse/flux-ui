5369d041de89ae7e92a3a54205829f684cb38f43
Samuel Corsi-House <chouse.samuel@gmail.com>
2022-03-19 23:57:58 -0400

    ci(misc): fix chromatic

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const tsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   stories: ['../packages/**/*.stories.tsx'],
   addons: [
@@ -12,5 +14,14 @@ module.exports = {
   },
   core: {
     builder: 'webpack5'
+  },
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new tsconfigPathsPlugin({
+        extensions: config.resolve.extensions
+      })
+    ];
+    return config;
   }
 };
