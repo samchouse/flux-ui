ec9eadf8f016d3ee82625c6d1363a131cdfffa7f
Samuel Corsi-House <chouse.samuel@gmail.com>
2022-03-20 00:07:43 -0400

    fix(checkbox): disabled variant (#43)
    * fix(checkbox): disabled variant
    
    * chore(misc): fix lint-staged
    
    * ci(misc): add chromatic workflow
    
    * ci(misc): fix chromatic
    
    * ci(misc): fix chromatic
    
    * ci(misc): fix chromatic
    
    * ci(misc): fix chromatic

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,18 @@
 module.exports = {
   '*.json': (filenames) =>
-    `prettier -w --ignore-path .prettierignore ${filenames.join(' ')}`,
+    `prettier -w --ignore-path .prettierignore ${filenames
+      .map((file) => file.split(`${process.cwd()}/`)[1])
+      .join(' ')}`,
   '*.{js,mjs,ts,tsx}': (filenames) =>
-    `eslint --ignore-path .gitignore ${filenames.join(' ')}`,
+    `eslint --ignore-path .gitignore ${filenames
+      .map((file) => file.split(`${process.cwd()}/`)[1])
+      .join(' ')}`,
   'packages/**/*.{ts,tsx}': (filenames) =>
-    `turbo run lint --scope=!@flux-ui/docs -- ${filenames.join(' ')}`,
+    `turbo run lint --scope=!@flux-ui/docs -- ${filenames
+      .map((file) => file.split(`${process.cwd()}/`)[1])
+      .join(' ')}`,
   'docs/**/*.{ts,tsx}': (filenames) =>
     `next lint --fix docs -c docs/.eslintrc --file ${filenames
-      .map((file) => file.split(process.cwd())[1])
+      .map((file) => file.split(`${process.cwd()}/`)[1])
       .join(' --file ')}`
 };
